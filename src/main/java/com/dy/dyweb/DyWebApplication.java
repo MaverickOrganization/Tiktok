90a1b06723a91b09d83d194d5ab22130ef15d755
xiangyuansong <xiangyuansong@ddpai.com>
2023-11-15 14:27:20 +0800

    初始化项目

---
+++ src/main/java/com/dy/dyweb/DyWebApplication.java
@@ -0,0 +1,13 @@
+package com.dy.dyweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DyWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DyWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dy/dyweb/DyWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dy.dyweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DyWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
