7be0df5ff500b79f6519530ee94eda1ada5bb812
vance <vance@txzing.com>
2023-11-19 13:23:50 +0800

    更换腾讯云子账号密匙遵循最小权限原则，更换ShapeableImageView

--- HttpClient/src/main/java/com/aking/tiktop/httpclient/config/AppConfig.kt
+++ HttpClient/src/main/java/com/aking/tiktop/httpclient/config/AppConfig.kt
@@ -5,9 +5,9 @@ object AppConfig {
     // 网络请求URL
     const val BASE_URL = "http://124.220.224.152:8080/"
     // 腾讯云密匙id
-    const val SECRETID = "AKIDSD7yjAXmz41zdF1mO6g9Y4a22wmW7ZwE"
+    const val SECRETID = "AKIDY3STStfHqPo6e04MnApu01A2woIkBkGl"
     // 腾讯云密匙key
-    const val SECRETKEY = "KoBQamswQvStuswvoSVn5qcGrQrIsQvI"
+    const val SECRETKEY = "VlhR3X0NF0bzEaAYpvrlV1SkvuiOvTzS"
     // 存储桶所属区域
     const val BUCKET = "ap-shanghai"
     // 存储桶名称
